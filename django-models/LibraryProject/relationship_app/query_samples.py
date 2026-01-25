8a467451d897b0a355c0b77809ecfbad351d1be6
joe <joelpartey123@gmail.com>
2026-01-25 09:40:52 -0800

    fixed errors

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,17 +1,12 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+# Query all books by a specific author
+author = Author.objects.get(name="John Doe")
+books_by_author = Book.objects.filter(author=author)
 
-def query_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.book_set.all()
+# List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
 
-
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
-
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
