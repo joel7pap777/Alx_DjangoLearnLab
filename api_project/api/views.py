33ed87ba1d96490616c5bf5f4e89a2b34b03a289
joe <joelpartey123@gmail.com>
2026-02-08 12:14:08 -0800

    Set up Book API endpoint with DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,22 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()      # Retrieves all books
     serializer_class = BookSerializer  # Uses the serializer we created
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
+
+# Existing ListAPIView
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Handles all CRUD operations for Book model:
+    - list, create, retrieve, update, destroy
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
